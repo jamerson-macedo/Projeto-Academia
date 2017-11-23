a40796bb281dd04740c3543306b79607c59346d9
estilizado <33670857+estilizado@users.noreply.github.com>
2017-11-23 10:39:23 -0300

    Add files via upload

--- projeto academia.c
+++ projeto academia.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <stdlib.h>
 // STRUCT
+typedef struct pagamentos{
+float janeiro;
+float fevereiro;
+float marco;
+float abri;
+float maio;
+float jun;
+float jul;
+float ago;
+float set;
+float out;
+float nov;
+float dez;
+}Pagamentos;
 typedef struct medidas{
 float biceps;
 float  triceps;
@@ -16,15 +30,15 @@ char nome[80];
 char sexo;
 int cpf;
 int idade;
-int celular;
+char celular[20];
 char cidade [30];
 char email[30];
 int data;
 Medidas medidas;
+Pagamentos pago;
 struct lista* prox;
 
 }Lista;
-
 
 // ASSINATURAS DAS FUNÇÕES
 
@@ -35,13 +49,15 @@ void desenvolvimentoAluno(void);
 int financeiro(void);
 int menuAluno(void);
 Lista* criarLista();
-Lista* inserir_dados(Lista *aluno,char *nome,char sexo,int cpf,int idade,int celular,char *cidade,char *email,int data);
+Lista* inserir_dados(Lista *aluno,char *nome,char sexo,int cpf,int idade,char *celular,char *cidade,char *email,int data);
 Lista* remover_cpf(Lista* Aluno,int valor);
 void Atualizar_dados(Lista* Aluno, int cpf);
-void imprimir_dados(Lista* Aluno);
-Lista* inserir_Medidas(Lista* Aluno, int cpf);
+void imprimir_dados(Lista *Aluno);
+Lista* inserir_Medidas(Lista* Aluno);
 void Saude_Aluno(Lista* Aluno,int cpf);
 void salvar(Lista* Aluno);
+int validaTelefone(char *telefone);
+int validaNome(char *cidade);
 // ESSA FUNÇÃO IRÁ CHAMAR A OUTRAS FUNÇÕES APENAS
 
 int main(void){
@@ -67,7 +83,7 @@ int main(void){
             Aluno=Dadospessoais(Aluno);
             break;
         case 2:
-            Aluno=inserir_Medidas(Aluno,cpf);
+            Aluno=inserir_Medidas(Aluno);
             break;
         case 3:
             printf("Informe o CPF para pesquisar : ");
@@ -77,7 +93,7 @@ int main(void){
         case 4:
             printf("Informe o CPF do aluno para excluir cadastro :");
             scanf("%d",&cpf);
-            remover_cpf(Aluno,cpf);
+            Aluno=remover_cpf(Aluno,cpf);
 
         }
         break;
@@ -148,6 +164,13 @@ Lista* Dadospessoais(Lista* Aluno){
 	Lista *novo=malloc(sizeof(Lista));
 	printf("Nome do Usuario : \n");
 	scanf(" %80[^\n]",novo->nome);
+	int val = validaNome(novo->nome);
+	while(val == 1){
+		printf("\nDigite o nome novamente: ");
+		scanf(" %80[^\n]", novo->nome);
+		fflush(stdin);
+		val = validaNome(novo->nome);
+		}
 	fflush(stdin);
 	printf("informe o sexo (M) ou (F) : \n");
 	scanf("%s",&novo->sexo);
@@ -158,8 +181,16 @@ Lista* Dadospessoais(Lista* Aluno){
 	printf("\ninforme a idade : ");
 	scanf("%d",&novo->idade);
 	fflush(stdin);
-	printf("\ninforme o celular : ");
-	scanf("%d",&novo->celular);
+	printf("\ninforme o celular: ");
+		scanf(" %11[^\n]", novo->celular);
+		fflush(stdin);
+		int val4 = validaTelefone(novo->celular);
+		while(val4 == 1){
+			printf("\nInforme o celular novamente: ");
+			scanf(" %11[^\n]", novo->celular);
+			fflush(stdin);
+			val4 = validaTelefone(novo->celular);
+		}
 	fflush(stdin);
 	printf("\ninforme a cidade : ");
 	scanf(" %80[^\n]",novo->cidade);
@@ -171,16 +202,20 @@ Lista* Dadospessoais(Lista* Aluno){
 	scanf("%d",&novo->data);
 	fflush(stdin);
 	printf("\n\nCADASTRO CONCLUIDO !! \n\n");
-	Aluno=inserir_dados(novo,novo->nome,novo->sexo,novo->cpf,novo->idade,novo->celular,novo->cidade,novo->email,novo->data);
+	Aluno=inserir_dados(Aluno,novo->nome,novo->sexo,novo->cpf,novo->idade,novo->celular,novo->cidade,novo->email,novo->data);
 	return Aluno;
 	}
 
-Lista* inserir_Medidas(Lista* Aluno, int cpf){
+Lista* inserir_Medidas(Lista* Aluno){
     int cpf1;
-    printf("\n\n~~ MEDIDAS~~\n\n");
+        printf("\n\n~~ MEDIDAS~~\n\n");
     printf("DIGITE O CPF PARA INSERIR AS MEDIDAS : ");
     scanf("%d",&cpf1);
     Lista* p;
+    if(Aluno==NULL){
+            printf("NÃO EXISTE ALUNO CADASTRADO !! ");
+    }
+
 for (p=Aluno;p!=NULL;p=p->prox){
     if(p->cpf==cpf1){
     printf("informe o Peso do Aluno : ");
@@ -220,7 +255,7 @@ for (p=Aluno;p!=NULL;p=p->prox){
 	scanf("%d",&p->idade);
 	fflush(stdin);
 	printf("\ninforme o celular : ");
-	scanf("%d",&p->celular);
+	scanf("%s",p->celular);
 	fflush(stdin);
 	printf("\ninforme a cidade : ");
 	scanf(" %80[^\n]",p->cidade);
@@ -232,32 +267,43 @@ for (p=Aluno;p!=NULL;p=p->prox){
 	scanf("%d",&p->data);
 	fflush(stdin);
     }
+    printf("CADASTRO ATUALIZADO");
 }
 }
 
 
-void imprimir_dados(Lista* Aluno){
-Lista* p;
+void imprimir_dados(Lista *Aluno){
+Lista* p=Aluno;
+int cont=0;
+if (p==NULL){
+        printf("vazio");
+        }
 for (p=Aluno;p!=NULL;p=p->prox){
 	printf("\n Nome do Usuario %s\n",p->nome);
-	printf("\n sexo %c",p->sexo);
-	printf("\ncpf %d",p->cpf);
-	printf("\nidade %d",p->idade);
-	printf("\ncelular %d",p->celular);
-	printf("\ncidade %s",p->cidade);
-	printf("\n E-MAIL %s",p->email);
-	printf("\ndata de Nascimento %d",p->data);
-
+	printf("\n sexo %c\n",p->sexo);
+	printf("\ncpf %d\n",p->cpf);
+	printf("\nidade %d\n",p->idade);
+	printf("\ncelular %s\n",p->celular);
+	printf("\ncidade %s\n",p->cidade);
+	printf("\n E-MAIL %s\n",p->email);
+	printf("\ndata de Nascimento %d\n",p->data);
+cont++;
 	}
+	printf("\n%d\n",cont);
 }
 
 void Saude_Aluno(Lista* Aluno,int cpf){
     int cpf2;
 	printf("\n\nSAUDE DO ALUNO\n\n");
 	printf("Informe o CPF do Aluno : ");
-	scanf("%d",&cpf2);
+    scanf("%d",&cpf2);
+    Lista* p;
+for (p=Aluno;p!=NULL;p=p->prox){
+    if(p->cpf==cpf2);
 
-	}
+// aqui comparar
+	};
+}
 
 void desenvolvimentoAluno(void){
 	printf("DESENVOLVIMENTO DE ALUNO!\n");
@@ -284,7 +330,7 @@ Lista* criarLista(){
 return NULL;
 
 }
-Lista* inserir_dados(Lista *aluno,char *nome,char sexo,int cpf,int idade,int celular,char *cidade,char *email,int data){
+Lista* inserir_dados(Lista *aluno,char *nome,char sexo,int cpf,int idade,char *celular,char *cidade,char *email,int data){
 Lista* p=aluno;
 // RECEBE A LISTA ALUNO
 Lista* ant=NULL;
@@ -292,16 +338,17 @@ Lista* ant=NULL;
 Lista* novo=(Lista*)malloc(sizeof(Lista));
 // adiciona nome
 strcpy(novo->nome, nome);
-novo->sexo;
+novo->sexo=sexo;
 novo->cpf=cpf;
 novo->idade=idade;
-novo->celular=celular;
+strcpy(novo->celular, celular);
 strcpy(novo->cidade, cidade);
 strcpy(novo->email,email);
 novo->data=data;
 novo->prox=NULL;
 // Lista vazia ?
 if (p==NULL){
+        printf("chegou em novo ");
         // guarda o novo
     return novo;
 }
@@ -313,30 +360,37 @@ while ((p!=NULL)&& strcmp(novo->nome,p->nome)>0) {
 }// nao entendi
  if (ant==NULL){
     novo->prox=p;
+    printf("chegou em novo2");
     return novo;
  }// novo prox aponta para ant que aponta para o proximo
  // entao o anterior agora aponta para novo
  novo->prox=ant->prox;
  ant->prox=novo;
+ printf("chegou em aluno");
  return aluno;
 
 }
 Lista* remover_cpf(Lista* Aluno,int valor){
 Lista* ant=NULL;
-Lista* p=Aluno; // p é aux pra percorrer
+Lista* p=Aluno;                  // p é aux pra percorrer
 while(p!=NULL && p->cpf!=valor){
     ant=p;
     p=p->prox;
+    printf("\n\nentrou no while\n\n");
 }// caso ELE NAO ACHAR----
-if (p==NULL)
+if (p==NULL){
     printf("ALUNO NÃO ENCONTRADO\n");
     return Aluno;
+}
 // ant é null so se for o primeiro elemento
 if(ant==NULL){
     Aluno=p->prox;
+    printf("chegou aqui ant null");
 }
-else
+else{
+    printf("chegou no se n");
     ant->prox=p->prox;
+}
 free(p);
 printf(" CADASTRO REMOVIDO COM SUCESSO !");
 return Aluno;
@@ -365,3 +419,25 @@ for(salva=Aluno;salva!=NULL;salva=salva->prox){
 }
 }
 }
+int validaNome(char* cidade){
+	int a;
+	for(a = 0; cidade[a] != '\0'; a++){
+		if((cidade[a] >= 'a' && cidade[a] <= 'z' ) || (cidade[a] >= 'A' && cidade[a] <= 'Z' ))
+			return 0;
+		else
+			return 1;
+		}
+	return 0;
+	}
+
+int validaTelefone(char *telefone){
+	int a;
+	int j = strlen(telefone);
+	for(a = 0; telefone[a] != '\0'; a++){
+		if((telefone[a] >= 65 && telefone[a] <= 90) || (telefone[a] >=97 && telefone[a] <= 122) || (j < 8))
+			return 1;
+		else
+			return 0;
+		}
+	return 0;
+	}
